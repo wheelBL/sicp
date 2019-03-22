2d96e522afb44566635a1bd1bb3757031f23b942
x00444734 <x00444734@huawei.com>
2019-03-22 16:01:33 +0800

    complete exercise 1.12

--- ch1.2.scm
+++ ch1.2.scm
@@ -33,3 +33,22 @@
 (f 2)
 (f 3)
 (f 4)
+
+;; exercise 1.12
+;; f(r,c) = 1, c == 0 or c > row - 1
+;;          f(r-1, c-1) + f(r-1, c), otherwise
+(define (pascal-triangle row col)
+  (if (or (= col 0) (> col (- row 1)))
+    1
+    (+ (pascal-triangle (- row 1) (- col 1))
+       (pascal-triangle (- row 1) col))))
+
+;; 1
+;; 1 1
+;; 1 2 1
+;; 1 3 3 1
+;; 1 4 6 4 1
+(pascal-triangle 0 0)
+(pascal-triangle 3 3)
+(pascal-triangle 3 2)
+(pascal-triangle 4 2)
