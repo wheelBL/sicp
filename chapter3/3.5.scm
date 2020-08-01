a2f61f49b8d089f2ca951628a50a248ebdbf935a
wheelBL <32828953+wheelBL@users.noreply.github.com>
2020-08-02 01:40:24 +0800

    Create 3.5.scm

---
+++ chapter3/3.5.scm
@@ -0,0 +1,24 @@
+(define (monte-carlo trials experiment)
+  (define (iter trials-remaining trials-passed)
+    (cond ((= trials-remaining 0)
+           (/ trials-passed trials))
+          ((experiment)
+           (iter (- trials-remaining 1) (+ trials-passed 1)))
+          (else
+           (iter (- trials-remaining 1) trials-passed))))
+  (iter trials 0))
+
+(define (random-in-range low high)
+  (let ((range (- high low)))
+    (+ low (random range))))
+
+(define (in-range x y)
+  (<= (+ (expt (- x 5) 2)
+         (expt (- y 7) 2))
+      (expt 3 2)))
+
+(define (estimate-integral p x1 x2 y1 y2 trials)
+  (define (experiment)
+    (p (random-in-range x1 x2)
+       (random-in-range y1 y2)))
+  (monte-carlo trials experiment))
